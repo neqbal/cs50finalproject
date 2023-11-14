0204553e9e205c59c13b96ec1042a3e4e211f49b
neqbal <nooraineqbal@gmail.com>
2023-11-15 01:33:25 +0530

    .

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 from flask_session import Session
-from flask_socketio import SocketIO
-from threading import Lock
 from werkzeug.security import generate_password_hash, check_password_hash 
 from helpers import apology, login_required
 from datetime import datetime
 from cs50 import SQL
 
 
-thread=None
-thread_lock=Lock()
 app = Flask(__name__)
 
 
@@ -17,7 +13,6 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"]="false"
 app.config["SESSION_TYPE"]="filesystem"
 Session(app)
-socketio=SocketIO(app)
 
 db = SQL("sqlite:///database.db")
 
@@ -31,29 +26,27 @@ def helloworld():
 def register():
 
     if request.method == "POST":
-        #sajsbdja
         
-        #ensure username was submitted
         if not request.form.get("username"):
             return apology("enter a username")
-        #ensure name was submitted
+        
         elif not request.form.get("name"):
             return apology("enter a name")
-        #ensure password was submitted
+        
         elif not request.form.get("password"):
             if not request.form.get("confirmation"):
                 return apology("must provide password")
-        #return apology if confirmation and password do not match
+        
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords do not match")
         
-        #ensure same username does not exist
+        
         users=db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         if len(users) == 1:
             return apology("username not available")
         else:
-            #insert user credentials into the database
+            
             db.execute("INSERT INTO users (name, username, hash) VALUES (?, ?, ?)",request.form.get("name"), request.form.get("username"), generate_password_hash(request.form.get("password")))        
 
 
@@ -68,20 +61,20 @@ def login():
     session.clear()
 
     if request.method == "POST":
-        #ensure if username was submitted
+        
         if not request.form.get("username"):
             return apology("must provide username")
-        #ensure if password was submitted
+        
         elif not request.form.get("password"):
             return apology("must provide password")
         
-        #ensure username exists and password is correct
+        
         row=db.execute("SELECT * FROM users WHERE username=?", request.form.get("username"))
 
         if len(row) != 1 or not check_password_hash(row[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password")
         
-        #remember which user has logged in
+    
         session["user_id"]=row[0]["id"]
         session["username"]=row[0]["username"]
 
